2d7a3917161242111d87291754ddf0df2f91a32f
Prashant Yadav <sanjicook14@gmail.com>
2025-06-19 21:09:31 +0530

    changes made

--- lib/widgets/main_text.dart
+++ lib/widgets/main_text.dart
@@ -10,14 +10,17 @@ class MainText extends StatelessWidget {
       top: screenWidth * 0.87,
       left: 0,
       right: 0,
-      child: Text(
-        'APPLICATION DEVELOPER',
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: screenWidth * 0.065,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 1.2,
+      child: Container(
+        height: 100,
+        child: Text(
+          'APPLICATION DEVELOPER',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: screenWidth * 0.065,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.2,
+          ),
         ),
       ),
     );
